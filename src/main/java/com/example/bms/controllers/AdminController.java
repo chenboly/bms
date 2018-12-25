76741be445d9f9d5a367de24c79adc15050c16d3
chenboly <chenboly@gmail.com>
2018-12-25 10:31:05 +0700

    update GUI

---
+++ src/main/java/com/example/bms/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package com.example.bms.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AdminController {
+    @GetMapping(value = {"/", "/home", "/index", "/admin"})
+    public String adminPage(){
+        return "admin/index";
+    }
+
+}
